1ca83cd98fab7651b183160df6021740ae554bed
ScottLightfoot <gslightfoot@gmail.com>
2020-09-04 10:09:24 -0500

    ex1 passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,10 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    out = None
+    for i in range(length):
+        if limit - weights[i] in weights[i+1:]:
+            return([weights[i+1:].index(limit-weights[i]) + i + 1, i])
+
 
     return None
